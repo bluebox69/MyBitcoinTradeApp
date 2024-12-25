6c4a65d1b4e0f795219301096d3d9a664b41169b
Paul Kogler <paulkogler@MBP-von-Paul.home>
2024-12-25 19:12:07 +0100

    set up icons,colors and fonts

--- app/src/main/java/com/example/mybitcointradeapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/mybitcointradeapp/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//My Colors
+val IceBlue = Color(0xFFD7ECFF)
+val LightYellow = Color(0xFFF8F6BC)
+val LightRed = Color(0xFFFFEAFE)
+val MyGreen = Color(0xFF5FD022)
+val MyRed = Color(0xFFD05F22)
+val LightWhite = Color(0xFFF8F8F8)
+val LightGrey = Color(0xFF969696)
+val LightBlack = Color(0xFF323232)
+
+
+
+
---
+++ app/src/main/java/com/example/mybitcointradeapp/ui/theme/FontFamilies.kt
@@ -0,0 +1,30 @@
+package com.example.mybitcointradeapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.example.mybitcointradeapp.R
+
+object FontFamilies {
+
+    val fontFamily1 = FontFamily(
+        Font(R.font.onest_thin, FontWeight.Thin),
+        Font(R.font.onest_light, FontWeight.Light),
+        Font(R.font.onest_regular, FontWeight.Normal),
+        Font(R.font.onest_medium, FontWeight.Medium),
+        Font(R.font.onest_semibold, FontWeight.SemiBold),
+        Font(R.font.onest_bold, FontWeight.Bold),
+        Font(R.font.onest_extrabold, FontWeight.ExtraBold),
+        Font(R.font.onest_extralight, FontWeight.ExtraLight),
+    )
+    val fontFamily2 = FontFamily(
+        Font(R.font.onest_thin, FontWeight.Thin),
+        Font(R.font.onest_light, FontWeight.Light),
+        Font(R.font.onest_regular, FontWeight.Normal),
+        Font(R.font.onest_medium, FontWeight.Medium),
+        Font(R.font.onest_semibold, FontWeight.SemiBold),
+        Font(R.font.onest_bold, FontWeight.Bold),
+        Font(R.font.onest_extrabold, FontWeight.ExtraBold),
+        Font(R.font.onest_extralight, FontWeight.ExtraLight),
+    )
+}
