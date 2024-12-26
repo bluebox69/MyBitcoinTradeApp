8500f4b0a5efabb9bdf565ee9f8c7d3b6c21d9b2
Paul Kogler <paulkogler@MBP-von-Paul.home>
2024-12-26 14:19:23 +0100

    fix repository error

--- app/src/main/java/com/example/mybitcointradeapp/data/remote/FrankfurterWechselkursAPI.kt
+++ app/src/main/java/com/example/mybitcointradeapp/data/remote/FrankfurterWechselkursAPI.kt
@@ -1,10 +1,10 @@
 package com.example.mybitcointradeapp.data.remote
 
-import com.example.mybitcointradeapp.domain.model.UsdToEurRate
+import com.example.mybitcointradeapp.data.remote.dto.UsdToEurDto
 import retrofit2.http.GET
 
 interface FrankfurterExchangeAPI {
 
     @GET("/v1/latest?base=USD")
-    suspend fun getUsdToEurRate(): UsdToEurRate
+    suspend fun getUsdToEurRate(): UsdToEurDto
 }
--- app/src/main/java/com/example/mybitcointradeapp/data/repository/UsdToEurRepositoryImpl.kt
+++ app/src/main/java/com/example/mybitcointradeapp/data/repository/UsdToEurRepositoryImpl.kt
@@ -1,14 +1,14 @@
 package com.example.mybitcointradeapp.data.repository
 
 import com.example.mybitcointradeapp.data.remote.FrankfurterExchangeAPI
-import com.example.mybitcointradeapp.domain.model.UsdToEurRate
+import com.example.mybitcointradeapp.data.remote.dto.UsdToEurDto
 import com.example.mybitcointradeapp.domain.repository.UsdToEurRateRepository
 import javax.inject.Inject
 
 class UsdToEurRepositoryImpl @Inject constructor(
     private val api: FrankfurterExchangeAPI
 ) : UsdToEurRateRepository  {
-    override suspend fun getUsdToEurRate(): UsdToEurRate {
+    override suspend fun getUsdToEurRate(): UsdToEurDto {
         return api.getUsdToEurRate()
     }
 }
